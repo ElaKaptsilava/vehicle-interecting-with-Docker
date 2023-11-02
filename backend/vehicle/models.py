a4cb755b31552e97311b659d892aa68f0cc37151
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2023-11-02 22:50:58 +0100

    create models: Vehicle, Rate

--- backend/vehicle/models.py
+++ backend/vehicle/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Manager(models.Model):
+    objects = models.Manager()
+
+    class Meta:
+        abstract = True
+
+
+class Vehicle(Manager):
+    make_ID = models.IntegerField()
+    make_name = models.CharField(max_length=100)
+    model_name = models.CharField(max_length=100)
+
+
+class Rate(Manager):
+    vehicle = models.ManyToManyField(Vehicle)
+    rate = models.IntegerField(default=0)
