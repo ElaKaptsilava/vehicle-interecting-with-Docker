bd64a1e41fe5fadf5606e9121d10c3988d01cb6c
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2023-11-02 23:00:09 +0100

    created viewssets, added filters

--- backend/vehicle/views.py
+++ backend/vehicle/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.viewsets import ModelViewSet
+from .models import Vehicle, Rate
+from .serializers import VehicleSerializer, RateSerializer
 
-# Create your views here.
+class RateViewSet(ModelViewSet):
+    serializer_class = RateSerializer
+    queryset = Rate.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['rate']
+
+
+class VehicleViewSet(ModelViewSet):
+    serializer_class = VehicleSerializer
+    queryset = Vehicle.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['model_name', 'make_name']
